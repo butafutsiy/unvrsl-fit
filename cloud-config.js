dfea44fe2db4a533f71fc430a665331b89a5fb97
butafutsiy <butak2000@gmail.com>
2026-08-26 09:28:12 +0800

    Connect UNVRSL FIT to Supabase

--- cloud-config.js
+++ cloud-config.js
@@ -1,7 +1,7 @@
 'use strict';
-// Заполним эти два публичных значения после создания проекта Supabase.
-// anonKey предназначен для клиентских приложений; service_role сюда НИКОГДА не вставлять.
+// Публичные данные проекта Supabase для UNVRSL FIT.
+// Здесь допустим только publishable/anon key. service_role сюда никогда не добавлять.
 window.UNVRSL_CLOUD=window.UNVRSL_CLOUD||{
-  url:'',
-  anonKey:''
+  url:'https://fibvmnkxvhthqopwknfv.supabase.co',
+  anonKey:'sb_publishable_Y8ctUkef4kq2Wu_GOJ23Rg_VCEmjqhe'
 };
