bd042c124858fb15b287b9b965f61ab5dc925c04
butafutsiy <butak2000@gmail.com>
2026-08-26 08:05:02 +0800

    Add cloud config placeholder

---
+++ cloud-config.js
@@ -0,0 +1,7 @@
+'use strict';
+// Заполним эти два публичных значения после создания проекта Supabase.
+// anonKey предназначен для клиентских приложений; service_role сюда НИКОГДА не вставлять.
+window.UNVRSL_CLOUD=window.UNVRSL_CLOUD||{
+  url:'',
+  anonKey:''
+};
