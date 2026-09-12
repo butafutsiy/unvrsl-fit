b8f8e0a4cd9de450c47f605e1feac92d10ba000b
butafutsiy <butak2000@gmail.com>
2026-09-13 00:13:19 +0800

    Refresh PWA cache for active method collapse

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_RELEASE='v367-even-rep-ranges';
-const STATIC_CACHE='unvrsl-static-v367';
+const SW_RELEASE='v369-active-method-collapse';
+const STATIC_CACHE='unvrsl-static-v369';
 const CORE_ASSETS=[
   './app.js?v=316',
   './startup-orchestrator-v260.js?v=321',
