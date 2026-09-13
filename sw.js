11a0cbf61561a6889df90dc5f3dcb49e0087915e
butafutsiy <butak2000@gmail.com>
2026-09-13 12:19:02 +0800

    Refresh PWA cache for rep range editor

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_RELEASE='v373-method-rep-authority';
-const STATIC_CACHE='unvrsl-static-v373';
+const SW_RELEASE='v384-method-ranges';
+const STATIC_CACHE='unvrsl-static-v384';
 const CORE_ASSETS=[
   './app.js?v=316',
   './startup-orchestrator-v260.js?v=321',
